a257b26be0b8322ccd3fa033f17c8c5cff3f99d7
Rishi Jha <rishijha568@gmail.com>
2025-09-22 14:58:44 +0530

    typo mistake fixed

--- server/config/database.js
+++ server/config/database.js
@@ -6,9 +6,9 @@ const { MONGODB_URL } = process.env;
 exports.connect = () => {
 	mongoose
 		.connect(MONGODB_URL)
-		.then(console.log(`DB Connection Success`))
+		.then(console.log("DB Connected Successfully"))
 		.catch((err) => {
-			console.log(`DB Connection Failed`);
+			console.log("DB Connection Failed");
 			console.log(err);
 			process.exit(1);
 		});
