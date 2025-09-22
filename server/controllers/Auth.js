943449875fccddd154b774e884bd9a8d5142f90e
Rishi Jha <161611926+rishijha235@users.noreply.github.com>
2025-09-23 01:07:06 +0530

    Fix return statement for cookie response in Auth.js

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -161,7 +161,7 @@ exports.login = async (req, res) => {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
         httpOnly: true,
       }
-      res.cookie("token", token, options).status(200).json({
+      return res.cookie("token", token, options).status(200).json({
         success: true,
         token,
         user,
