750a61745db9cc458982118b0518611d3e09bf48
Rishi Jha <161611926+rishijha235@users.noreply.github.com>
2025-09-23 00:53:14 +0530

    Fix CORS origin string formatting

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: https://studynotion-kappa-eight.vercel.app/,
+		origin: "https://studynotion-kappa-eight.vercel.app",
 		credentials: true,
 	})
 );
